53c69063c7a84789d98bd3729f539910b87a2c68
Cat <89495053+aRealCatTBH@users.noreply.github.com>
2023-02-24 12:38:54 -0500

    Create smoothscroll.js

---
+++ smoothscroll.js
@@ -0,0 +1,18 @@
+window.onhashchange = scroll;
+
+document.getElementsByTagName("html")[0].style.scrollBehavior = "smooth";
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", scroll);
+});
+
+function scroll(e) {
+    if (typeof Element.prototype.scrollIntoView == "function") {
+        e.preventDefault();
+        document.getElementById(location.hash.replace("#", "")).scrollIntoView({
+            behavior: "smooth",
+        });
+    }
+}
+
+scroll();
